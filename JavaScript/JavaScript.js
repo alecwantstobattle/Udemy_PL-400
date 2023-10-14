12995f1dd53175c64be72245b97e70d3b28de694
Alec Tecson <tecsonalr@gmail.com>
2023-10-14 22:40:15 +0800

    12. 45. Setting and getting the value fields

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,3 +1,9 @@
 this.formOnLoad = function (executionContext) {
-    var formContext = executionContext.getFormContext();    
+    var formContext = executionContext.getFormContext();
+    formContext.ui.setFormNotification("Hello World v3", "INFO", "IDUnique14102023");
+    if (formContext.getAttribute("fax").getValue() == null)
+    {
+        formContext.getAttribute("fax").setValue("123-4567");
+    }
+    
 }
